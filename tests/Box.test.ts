ba44f74781b0b9067f73c871188b383f5d5809ab
darellanodev <darellano.aliaga@gmail.com>
2025-05-03 15:09:16 +0200

    feat: Create the Box component and print the title (#6)

---
+++ tests/Box.test.ts
@@ -0,0 +1,24 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { expect, test } from "vitest";
+import Box from "../src/components/Box.astro";
+import type { Bookmark } from "../src/components/Bookmark.astro";
+
+test("should render the title of the Box and their two bookmarks", async () => {
+  const container = await AstroContainer.create();
+
+  const bookmarks: Bookmark[] = [
+    { url: "https://codewars.com" },
+    { url: "https://devdocs.io" },
+  ];
+
+  const result = await container.renderToString(Box, {
+    props: {
+      box: {
+        title: "Katas",
+        bookmarks: bookmarks,
+      },
+    },
+  });
+
+  expect(result).toContain("Katas");
+});
