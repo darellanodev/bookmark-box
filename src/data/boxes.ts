07d8dbc2201dd2ebdfdd665dc81f0c0e9664dbb3
darellanodev <darellano.aliaga@gmail.com>
2025-05-11 21:21:10 +0200

    refactor: Move the data of the boxes to data/boxes.ts (closes #10)

---
+++ src/data/boxes.ts
@@ -0,0 +1,18 @@
+export const katasBox = {
+  title: "Katas",
+  bookmarks: [
+    { url: "https://codewars.com" },
+    { url: "https://devdocs.io" },
+    { url: "https://perplexity.ai" },
+  ],
+};
+
+export const videoBox = {
+  title: "Video",
+  bookmarks: [
+    { url: "https://github.com/yt-dlp/yt-dlp/releases" },
+    { url: "https://online-video-cutter.com/" },
+    { url: "https://www.capcut.com/es-es/" },
+    { url: "https://clideo.com/" },
+  ],
+};
