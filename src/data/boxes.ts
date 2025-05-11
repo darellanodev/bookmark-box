a3b0211b29525612992cbbe21917b6819f6308e7
darellanodev <darellano.aliaga@gmail.com>
2025-05-11 21:36:51 +0200

    feat: Add more boxes (closes #12)

--- src/data/boxes.ts
+++ src/data/boxes.ts
@@ -1,18 +1,98 @@
 export const katasBox = {
-  title: "Katas",
-  bookmarks: [
-    { url: "https://codewars.com" },
-    { url: "https://devdocs.io" },
-    { url: "https://perplexity.ai" },
-  ],
-};
+  title: 'Katas',
+  bookmarks: [{ url: 'https://codewars.com' }, { url: 'https://devdocs.io' }, { url: 'https://perplexity.ai' }],
+}
 
 export const videoBox = {
-  title: "Video",
+  title: 'Video',
   bookmarks: [
-    { url: "https://github.com/yt-dlp/yt-dlp/releases" },
-    { url: "https://online-video-cutter.com/" },
-    { url: "https://www.capcut.com/es-es/" },
-    { url: "https://clideo.com/" },
+    { url: 'https://github.com/yt-dlp/yt-dlp/releases' },
+    { url: 'https://online-video-cutter.com/' },
+    { url: 'https://www.capcut.com/es-es/' },
+    { url: 'https://clideo.com/' },
   ],
-};
+}
+
+export const codeBox = {
+  title: 'Code',
+  bookmarks: [
+    { url: 'https://codeium.com/live/general' },
+    { url: 'https://phpsandbox.io/' },
+    { url: 'https://playcode.io/' },
+  ],
+}
+
+export const designBox = {
+  title: 'Design',
+  bookmarks: [
+    { url: 'https://excalidraw.com/' },
+    { url: 'https://www.figma.com/' },
+    { url: 'https://boxy-svg.com/app' },
+    { url: 'https://www.svgrepo.com/' },
+    { url: 'https://perfect-freehand-example.vercel.app/' },
+    { url: 'https://www.fffuel.co/' },
+  ],
+}
+
+export const toolsBox = {
+  title: 'Tools',
+  bookmarks: [
+    { url: 'https://dbdiagram.io/' },
+    { url: 'https://app.diagrams.net/' },
+    { url: 'https://online.visual-paradigm.com/drive/#proj=0&dashboard' },
+  ],
+}
+
+export const learningBox = {
+  title: 'Learning',
+  bookmarks: [
+    { url: 'https://regexlearn.com/learn/regex101' },
+    { url: 'https://go.dev/tour/flowcontrol/1' },
+    { url: 'https://go.dev/tour/basics/1' },
+  ],
+}
+
+export const workBox = {
+  title: 'Work',
+  bookmarks: [
+    { url: 'https://rxresu.me/' },
+    { url: 'https://www.linkedin.com/feed/' },
+    { url: 'https://www.infojobs.net/' },
+  ],
+}
+
+export const personalBox = {
+  title: 'Personal',
+  bookmarks: [
+    { url: 'https://darellanodev.github.io/' },
+    { url: 'https://github.com/darellanodev/' },
+    { url: 'https://keep.google.com/#home' },
+    { url: 'https://calendar.google.com/calendar/u/0/r' },
+    { url: 'https://mail.google.com/mail/u/0/?hl=es#inbox' },
+    { url: 'https://discord.com' },
+  ],
+}
+
+export const musicBox = {
+  title: 'Music',
+  bookmarks: [{ url: 'https://signal.vercel.app/edit' }],
+}
+
+export const IABox = {
+  title: 'IA',
+  bookmarks: [
+    { url: 'https://www.perplexity.ai/' },
+    { url: 'https://you.com/' },
+    { url: 'https://huggingface.co/' },
+    { url: 'https://replicate.com/' },
+    { url: 'https://www.phind.com/search?home=true' },
+  ],
+}
+
+export const moviesBox = {
+  title: 'Movies',
+  bookmarks: [
+    { url: 'https://www.filmaffinity.com/es/main.html' },
+    { url: 'https://www.teletexto.com/teletexto.asp?programacion=peliculas' },
+  ],
+}
