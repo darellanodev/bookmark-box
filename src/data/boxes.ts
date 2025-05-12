79f2c2a364cc4089ac64dfee44858263fda9cbd8
darellanodev <darellano.aliaga@gmail.com>
2025-05-12 21:15:54 +0200

    feat: Add a title to every URL and fix test (closes #15)

--- src/data/boxes.ts
+++ src/data/boxes.ts
@@ -1,98 +1,102 @@
 export const katasBox = {
   title: 'Katas',
-  bookmarks: [{ url: 'https://codewars.com' }, { url: 'https://devdocs.io' }, { url: 'https://perplexity.ai' }],
+  bookmarks: [
+    { title: 'Codewars', url: 'https://codewars.com' },
+    { title: 'DevDocs', url: 'https://devdocs.io' },
+    { title: 'Perplexity', url: 'https://perplexity.ai' },
+  ],
 }
 
 export const videoBox = {
   title: 'Video',
   bookmarks: [
-    { url: 'https://github.com/yt-dlp/yt-dlp/releases' },
-    { url: 'https://online-video-cutter.com/' },
-    { url: 'https://www.capcut.com/es-es/' },
-    { url: 'https://clideo.com/' },
+    { title: 'yt-dlp', url: 'https://github.com/yt-dlp/yt-dlp/releases' },
+    { title: 'Online Video Cutter', url: 'https://online-video-cutter.com/' },
+    { title: 'CapCut', url: 'https://www.capcut.com/es-es/' },
+    { title: 'Clideo', url: 'https://clideo.com/' },
   ],
 }
 
 export const codeBox = {
   title: 'Code',
   bookmarks: [
-    { url: 'https://codeium.com/live/general' },
-    { url: 'https://phpsandbox.io/' },
-    { url: 'https://playcode.io/' },
+    { title: 'Codeium', url: 'https://codeium.com/live/general' },
+    { title: 'PHP Sandbox', url: 'https://phpsandbox.io/' },
+    { title: 'Playcode', url: 'https://playcode.io/' },
   ],
 }
 
 export const designBox = {
   title: 'Design',
   bookmarks: [
-    { url: 'https://excalidraw.com/' },
-    { url: 'https://www.figma.com/' },
-    { url: 'https://boxy-svg.com/app' },
-    { url: 'https://www.svgrepo.com/' },
-    { url: 'https://perfect-freehand-example.vercel.app/' },
-    { url: 'https://www.fffuel.co/' },
+    { title: 'Excalidraw', url: 'https://excalidraw.com/' },
+    { title: 'Figma', url: 'https://www.figma.com/' },
+    { title: 'Boxy SVG', url: 'https://boxy-svg.com/app' },
+    { title: 'SVG Repo', url: 'https://www.svgrepo.com/' },
+    { title: 'Perfect Freehand', url: 'https://perfect-freehand-example.vercel.app/' },
+    { title: 'FFFuel', url: 'https://www.fffuel.co/' },
   ],
 }
 
 export const toolsBox = {
   title: 'Tools',
   bookmarks: [
-    { url: 'https://dbdiagram.io/' },
-    { url: 'https://app.diagrams.net/' },
-    { url: 'https://online.visual-paradigm.com/drive/#proj=0&dashboard' },
+    { title: 'DB Diagram', url: 'https://dbdiagram.io/' },
+    { title: 'Diagrams.net', url: 'https://app.diagrams.net/' },
+    { title: 'Visual Paradigm', url: 'https://online.visual-paradigm.com/drive/#proj=0&dashboard' },
   ],
 }
 
 export const learningBox = {
   title: 'Learning',
   bookmarks: [
-    { url: 'https://regexlearn.com/learn/regex101' },
-    { url: 'https://go.dev/tour/flowcontrol/1' },
-    { url: 'https://go.dev/tour/basics/1' },
+    { title: 'Regex Learn', url: 'https://regexlearn.com/learn/regex101' },
+    { title: 'Go Tour', url: 'https://go.dev/tour/flowcontrol/1' },
+    { title: 'Go Tour Basics', url: 'https://go.dev/tour/basics/1' },
   ],
 }
 
 export const workBox = {
   title: 'Work',
   bookmarks: [
-    { url: 'https://rxresu.me/' },
-    { url: 'https://www.linkedin.com/feed/' },
-    { url: 'https://www.infojobs.net/' },
+    { title: 'Rxresu.me', url: 'https://rxresu.me/' },
+    { title: 'LinkedIn', url: 'https://www.linkedin.com/feed/' },
+    { title: 'InfoJobs', url: 'https://www.infojobs.net/' },
   ],
 }
 
 export const personalBox = {
   title: 'Personal',
   bookmarks: [
-    { url: 'https://darellanodev.github.io/' },
-    { url: 'https://github.com/darellanodev/' },
-    { url: 'https://keep.google.com/#home' },
-    { url: 'https://calendar.google.com/calendar/u/0/r' },
-    { url: 'https://mail.google.com/mail/u/0/?hl=es#inbox' },
-    { url: 'https://discord.com' },
+    { title: 'Portfolio', url: 'https://darellanodev.github.io/' },
+    { title: 'GitHub', url: 'https://github.com/darellanodev/' },
+    { title: 'Keep', url: 'https://keep.google.com/#home' },
+    { title: 'Calendar', url: 'https://calendar.google.com/calendar/u/0/r' },
+    { title: 'Mail', url: 'https://mail.google.com/mail/u/0/?hl=es#inbox' },
+    { title: 'Discord', url: 'https://discord.com' },
   ],
 }
 
 export const musicBox = {
   title: 'Music',
-  bookmarks: [{ url: 'https://signal.vercel.app/edit' }],
+  bookmarks: [{ title: 'Signal', url: 'https://signal.vercel.app/edit' }],
 }
 
 export const IABox = {
   title: 'IA',
   bookmarks: [
-    { url: 'https://www.perplexity.ai/' },
-    { url: 'https://you.com/' },
-    { url: 'https://huggingface.co/' },
-    { url: 'https://replicate.com/' },
-    { url: 'https://www.phind.com/search?home=true' },
+    { title: 'Perplexity', url: 'https://www.perplexity.ai/' },
+    { title: 'You.com', url: 'https://you.com/' },
+    { title: 'Hugging Face', url: 'https://huggingface.co/' },
+    { title: 'Replicate', url: 'https://replicate.com/' },
+    { title: 'Phind', url: 'https://www.phind.com/search?home=true' },
   ],
 }
 
 export const moviesBox = {
   title: 'Movies',
   bookmarks: [
-    { url: 'https://www.filmaffinity.com/es/main.html' },
-    { url: 'https://www.teletexto.com/teletexto.asp?programacion=peliculas' },
+    { title: 'FilmAffinity', url: 'https://www.filmaffinity.com/es/main.html' },
+    { title: 'Teletexto', url: 'https://www.teletexto.com/teletexto.asp?programacion=peliculas' },
   ],
 }
