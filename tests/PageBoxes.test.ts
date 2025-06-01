941d7f1e030130bc3aa8767ab3c1d251fa44c528
darellanodev <darellano.aliaga@gmail.com>
2025-06-01 21:30:18 +0200

    refactor: Fix the english descriptions of the PageBoxes tests (closes #17)

--- tests/PageBoxes.test.ts
+++ tests/PageBoxes.test.ts
@@ -7,67 +7,68 @@ const renderPageBoxes = async () => {
   return await container.renderToString(PageBoxes)
 }
 
-test('should render the title of the Box Katas', async () => {
+test('should render the title of the Katas box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Katas')
 })
 
-test('should render the title of the Box Video', async () => {
+test('should render the title of the Video box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Video')
 })
 
-test('should render the title of the Box Code', async () => {
+test('should render the title of the Code box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Code')
 })
 
-test('should render the title of the Box Design', async () => {
+test('should render the title of the Design box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Design')
 })
 
-test('should render the title of the Box Tools', async () => {
+test('should render the title of the Tools box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Tools')
 })
 
-test('should render the title of the Box Learning', async () => {
+test('should render the title of the Learning box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Learning')
 })
 
-test('should render the title of the Box Work', async () => {
+test('should render the title of the Work box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Work')
 })
 
-test('should render the title of the Box Personal', async () => {
+test('should render the title of the Personal box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Personal')
 })
 
-test('should render the title of the Box Music', async () => {
+test('should render the title of the Music box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Music')
 })
 
-test('should render the title of the Box IA', async () => {
+test('should render the title of the IA box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('IA')
 })
 
-test('should render the title of the Box Movies', async () => {
+test('should render the title of the Movies box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Movies')
 })
 
-test('should render the title of the Box GitHub', async () => {
+test('should render the title of the GitHub box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('GitHub')
 })
 
-test('should render the title of the Box Images AI', async () => {
+test('should render the title of the Images AI box', async () => {
   const result = await renderPageBoxes()
   expect(result).toContain('Images AI')
 })
+
