4975440edcb0c87d163fa010fdaa6c105dd6a465
darellanodev <darellano.aliaga@gmail.com>
2025-06-07 11:48:39 +0200

    feat: Change NavLink astro component to a tsx component (#24)

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import type { AnchorHTMLAttributes } from 'react'
+
+interface NavLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
+
+const NavLink = ({ href, children, ...props }: NavLinkProps) => {
+  return (
+    <a href={href} className="mx-6 hover:bg-gray-500 px-3 py-2 rounded-lg" {...props}>
+      {children}
+    </a>
+  )
+}
+
+export default NavLink
