00b8f8948310c40e00f341fb728c918303e89b55
darellanodev <darellano.aliaga@gmail.com>
2025-06-07 11:50:32 +0200

    test: Change the astro test to be a react test (#24)

---
+++ tests/NavLink.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import { test, expect } from 'vitest'
+import NavLink from '../src/components/NavLink'
+
+test('renders the href and class correctly', () => {
+  render(<NavLink href="/about">About</NavLink>)
+
+  const linkElement = screen.getByRole('link', { name: /about/i })
+  expect(linkElement).toHaveAttribute('href', '/about')
+  expect(linkElement).toHaveClass('mx-6')
+})
