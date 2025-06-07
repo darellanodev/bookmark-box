63767a2ead8358790fefed390bfd4b78455f478f
darellanodev <darellano.aliaga@gmail.com>
2025-06-07 12:41:27 +0200

    fix: Remove the extension of the Header in the import statement (#24)

--- tests/Header.test.tsx
+++ tests/Header.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import { test, expect } from 'vitest'
-import Header from '../src/components/Header.jsx'
+import Header from '../src/components/Header'
 
 test('renders an input to filter boxes', () => {
   render(<Header />)
