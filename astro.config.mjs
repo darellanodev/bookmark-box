cf4305045055ae3d460ebf6315780d7f2c3ed47d
darellanodev <darellano.aliaga@gmail.com>
2025-06-07 12:51:35 +0200

    chore: Format astro.config.mjs with prettier (#24)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://darellanodev.github.io/bookmark-box",
-  base: "/bookmark-box/",
-  trailingSlash: "ignore",
-});
+  site: 'https://darellanodev.github.io/bookmark-box',
+  base: '/bookmark-box/',
+  trailingSlash: 'ignore',
+})
