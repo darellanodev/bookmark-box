c5806247cf1e4051b6ae8a6f1c1d95dd392b99fb
darellanodev <darellano.aliaga@gmail.com>
2025-06-07 12:29:52 +0200

    fix: Use a level heading for the tests (#24)

--- tests/PageBoxes.test.tsx
+++ tests/PageBoxes.test.tsx
@@ -4,80 +4,75 @@ import PageBoxes from '../src/components/PageBoxes.jsx'
 
 test('should render the title of the Katas box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/Katas/i)).toBeInTheDocument()
-})
-
-test('should render the title of the Video box', () => {
-  render(<PageBoxes />)
-  expect(screen.getByText(/Video/i)).toBeInTheDocument()
-})
-
-test('should render the title of the Code box', () => {
-  render(<PageBoxes />)
-  expect(screen.getByText(/Code/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /Katas/i, level: 2 })).toBeInTheDocument()
 })
 
 test('should render the title of the Design box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/Design/i)).toBeInTheDocument()
-})
-
-test('should render the title of the Tools box', () => {
-  render(<PageBoxes />)
-  expect(screen.getByText(/Tools/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /Design/i, level: 2 })).toBeInTheDocument()
 })
 
 test('should render the title of the Learning box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/Learning/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /Learning/i, level: 2 })).toBeInTheDocument()
 })
 
 test('should render the title of the Work box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/Work/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /Work/i, level: 2 })).toBeInTheDocument()
 })
 
 test('should render the title of the Personal box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/Personal/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /Personal/i, level: 2 })).toBeInTheDocument()
 })
 
 test('should render the title of the Music box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/Music/i)).toBeInTheDocument()
-})
-
-test('should render the title of the IA box', () => {
-  render(<PageBoxes />)
-  expect(screen.getByText(/IA/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /Music/i, level: 2 })).toBeInTheDocument()
 })
 
 test('should render the title of the Movies box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/Movies/i)).toBeInTheDocument()
-})
-
-test('should render the title of the GitHub box', () => {
-  render(<PageBoxes />)
-  expect(screen.getByText(/GitHub/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /Movies/i, level: 2 })).toBeInTheDocument()
 })
 
 test('should render the title of the Images AI box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/Images AI/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /Images AI/i, level: 2 })).toBeInTheDocument()
 })
 
 test('should render the title of the PDF readers box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/PDF readers/i)).toBeInTheDocument()
-})
-
-test('should render the title of the Git box', () => {
-  render(<PageBoxes />)
-  expect(screen.getByText(/Git/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /PDF readers/i, level: 2 })).toBeInTheDocument()
 })
 
 test('should render the title of the Diagramming tools box', () => {
   render(<PageBoxes />)
-  expect(screen.getByText(/Diagramming tools/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { name: /Diagramming tools/i, level: 2 })).toBeInTheDocument()
+})
+
+test('should render the title of the Video box', () => {
+  render(<PageBoxes />)
+  expect(screen.getByRole('heading', { name: /Video/i, level: 2 })).toBeInTheDocument()
+})
+
+test('should render the title of the Code box', () => {
+  render(<PageBoxes />)
+  expect(screen.getByRole('heading', { name: /Code/i, level: 2 })).toBeInTheDocument()
+})
+
+test('should render the title of the Git box', () => {
+  render(<PageBoxes />)
+  expect(screen.getByRole('heading', { name: /^Git$/i, level: 2 })).toBeInTheDocument()
+})
+
+test('should render the title of the IA box', () => {
+  render(<PageBoxes />)
+  expect(screen.getByRole('heading', { name: /^IA$/i, level: 2 })).toBeInTheDocument()
+})
+
+test('should render the title of the Tools box', () => {
+  render(<PageBoxes />)
+  expect(screen.getByRole('heading', { name: /^Tools$/i, level: 2 })).toBeInTheDocument()
 })
